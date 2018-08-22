bcecdcf891f7703357a5ab94b4f47045b3e34208
Sourqe <nickvandewaterlaat@hotmail.com>
2018-08-22 09:50:51 +0200

    added basic components

---
+++ src/components/Remote.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-list-tile avatar>
+    <v-list-tile-avatar></v-list-tile-avatar>
+    <v-list-tile-content>remote content</v-list-tile-content>
+  </v-list-tile>
+</template>
+
+<script>    
+export default {
+    
+}
+</script>
---
+++ src/components/RemotesList.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-list two-line>
+    <v-subheader>Remotes List</v-subheader>
+    <hb-remote></hb-remote>
+  </v-list>
+</template>
+
+<script>
+import HbRemote from './Remote.vue';
+
+export default {
+  components: {
+    HbRemote
+  }
+}
+</script>
